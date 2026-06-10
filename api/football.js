a9242ca7616a7a424bfb86d8844e27f2530ae656
yr9ypnn22m-dotcom <yr9ypnn22m@privaterelay.appleid.com>
2026-06-10 16:54:24 +0200

    Add files via upload

--- api/football.js
+++ api/football.js
@@ -1,13 +1,18 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(
-      "https://api.football-data.org/v4/competitions/WC",
-      {
-        headers: {
-          "X-Auth-Token": process.env.FOOTBALL_API_KEY
-        }
+    const limit = req.query.limit;
+
+    let url = "https://api.football-data.org/v4/competitions/WC/matches?season=2026";
+
+    if (limit) {
+      url += `&limit=${encodeURIComponent(limit)}`;
+    }
+
+    const response = await fetch(url, {
+      headers: {
+        "X-Auth-Token": process.env.FOOTBALL_API_KEY
       }
-    );
+    });
 
     const text = await response.text();
 
